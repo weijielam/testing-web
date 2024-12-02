e79c6a545a6c4b53628e38237dfd5ea483bb4a15
Wei Jie Lam <weijielam@gmail.com>
2024-12-02 06:33:13 +0000

    Adding Base Code

---
+++ src/test/java/com/example/testing_web/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
